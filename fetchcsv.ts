0f4adf983ff17ce350e92b4271203d7742d52dcf
M L <max.leo2000@gmail.com>
2022-09-17 18:58:29 +0200

    error handling

--- fetchcsv.ts
+++ fetchcsv.ts
@@ -3,6 +3,12 @@ import { parseCsv } from "./deps.ts";
 export const fetchCSV = async (url: string, encoding = 'utf-8', seperator = ',') => {
     const decoder = new TextDecoder(encoding)
     const file = await (await fetch(url)).arrayBuffer();
-    const content = await parseCsv(decoder.decode(file), {separator: seperator});
-    return content;
+    try {
+        const content = await parseCsv(decoder.decode(file), {separator: seperator});
+        return content;
+    } catch (e) {
+        console.log("An error occured while trying to parse the csv file.");
+        console.error(e);
+    }
+    return [];
 }
