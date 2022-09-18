cdfdb7de3926aa34f1c693fed17c998a97e21eb5
M L <max.leo2000@gmail.com>
2022-09-18 09:29:50 +0200

    bug fix

--- fetchcsv.ts
+++ fetchcsv.ts
@@ -1,14 +1,14 @@
 import { parseCsv } from "./deps.ts";
-
 /*
 v0.2
 */
 
 export const fetchCSV = async (url: string, encoding = 'utf-8', seperator = ',') => {
-    const decoder = new TextDecoder(encoding)
+    // const decoder = new TextDecoder(encoding)
     const file = await (await fetch(url)).arrayBuffer();
+    const dc = new TextDecoder()
     try {
-        const content = await parseCsv(decoder.decode(file), {separator: seperator});
+        const content = await parseCsv(dc.decode(file), {separator: seperator});
         return content;
     } catch (e) {
         console.log("An error occured while trying to parse the csv file.");
